60f68d54575f4fc80ccb39a761e8402a1754060a
zhangchunsheng <zhangchunsheng423@gmail.com>
2013-10-16 23:19:38 +0800

    1、新功能：初始化项目

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ seaking_serverTest
 ==================
 
 test seaking server
+
+压力测试
+通过系统任务，生成系统命令，调用系统进程
---
+++ app.js
@@ -0,0 +1,12 @@
+/**
+ * Author: ZhangChunsheng
+ * Date: 2013-10-16
+ * Description: app
+ */
+function main() {
+    console.log("hello");
+
+    process.exit(0);
+}
+
+main();
