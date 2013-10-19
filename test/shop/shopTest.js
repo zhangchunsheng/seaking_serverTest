ba733aaa7b41561e2e65ca95dbbcd44942950d21
zhangchunsheng <zhangchunsheng423@gmail.com>
2013-10-20 01:12:47 +0800

    1、新功能：增加ab测试

---
+++ test/shop/shopTest.js
@@ -0,0 +1,49 @@
+/**
+ * Author: ZhangChunsheng
+ * Date: 2013-10-19
+ * Description: battleTest
+ */
+var spawn = require('child_process').spawn,
+    exec = require('child_process').exec,
+    os = require('os'),
+    utils = require('../../app/utils/utils'),
+    abUtil = require('../../app/utils/abUtil'),
+    consts = require('../../app/consts/consts');
+
+var serverConfig = require('../../config/server');
+var env = process.env.NODE_ENV || 'development';
+if(serverConfig[env]) {
+    serverConfig = serverConfig[env];
+}
+
+var shopTest = module.exports;
+
+shopTest.buyItem = function(data) {
+    abUtil.getRandomCookie(function(cookie) {
+        exec(utils.makeABCommand({
+            verbosity: serverConfig.abTest[consts.serverType.seaking_server].verbosity,
+            requests: serverConfig.abTest[consts.serverType.seaking_server].requests,
+            concurrency: serverConfig.abTest[consts.serverType.seaking_server].concurrency,
+            cookie: cookie,
+            url: abUtil.getCreateMainPlayerUrl(data),
+            output: abUtil.getOutputFile("createMainPlayer")
+        }), function(err, stdout, stderr) {
+            console.log(stdout);
+        });
+    });
+}
+
+shopTest.sellItem = function(data) {
+    abUtil.getRandomCookie(function(cookie) {
+        exec(utils.makeABCommand({
+            verbosity: serverConfig.abTest[consts.serverType.seaking_server].verbosity,
+            requests: serverConfig.abTest[consts.serverType.seaking_server].requests,
+            concurrency: serverConfig.abTest[consts.serverType.seaking_server].concurrency,
+            cookie: cookie,
+            url: abUtil.getCreateMainPlayerUrl(data),
+            output: abUtil.getOutputFile("createMainPlayer")
+        }), function(err, stdout, stderr) {
+            console.log(stdout);
+        });
+    });
+}
---
+++ test/task/taskTest.js
@@ -0,0 +1,49 @@
+/**
+ * Author: ZhangChunsheng
+ * Date: 2013-10-19
+ * Description: battleTest
+ */
+var spawn = require('child_process').spawn,
+    exec = require('child_process').exec,
+    os = require('os'),
+    utils = require('../../app/utils/utils'),
+    abUtil = require('../../app/utils/abUtil'),
+    consts = require('../../app/consts/consts');
+
+var serverConfig = require('../../config/server');
+var env = process.env.NODE_ENV || 'development';
+if(serverConfig[env]) {
+    serverConfig = serverConfig[env];
+}
+
+var taskTest = module.exports;
+
+taskTest.startTask = function(data) {
+    abUtil.getRandomCookie(function(cookie) {
+        exec(utils.makeABCommand({
+            verbosity: serverConfig.abTest[consts.serverType.seaking_server].verbosity,
+            requests: serverConfig.abTest[consts.serverType.seaking_server].requests,
+            concurrency: serverConfig.abTest[consts.serverType.seaking_server].concurrency,
+            cookie: cookie,
+            url: abUtil.getCreateMainPlayerUrl(data),
+            output: abUtil.getOutputFile("createMainPlayer")
+        }), function(err, stdout, stderr) {
+            console.log(stdout);
+        });
+    });
+}
+
+taskTest.handOverTask = function(data) {
+    abUtil.getRandomCookie(function(cookie) {
+        exec(utils.makeABCommand({
+            verbosity: serverConfig.abTest[consts.serverType.seaking_server].verbosity,
+            requests: serverConfig.abTest[consts.serverType.seaking_server].requests,
+            concurrency: serverConfig.abTest[consts.serverType.seaking_server].concurrency,
+            cookie: cookie,
+            url: abUtil.getCreateMainPlayerUrl(data),
+            output: abUtil.getOutputFile("createMainPlayer")
+        }), function(err, stdout, stderr) {
+            console.log(stdout);
+        });
+    });
+}
